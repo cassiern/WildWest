bf1b6525e3f83fcb485d34101c50e2203e789677
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-07-11 12:48:24 -0600

    html and javascript complete

--- js/app.js
+++ js/app.js
@@ -8,129 +8,251 @@ $('#myModal').on('shown.bs.modal', function () {
 
 
 
-//Source for progress Bar || https://www.youtube.com/watch?v=QIvFUjsXlJI
-function progress(){
-	const prg = $('#progress');
-	const percent = $('#percentCount');
-	let counter = 0;
-	const progress = 25;
-	const id = setInterval(frame, 1000);
 
-	function frame(){
-		if(progress === 30000 && counter === 30000){
-			clearInterval(id);
-		}else {
-			progress+= 5;
-			counter+= 1;
-			prg.style.width = progress + 'px';
-			percent.innerHTML = counter + '%';
-		}
-	}
+const progress = document.getElementById('progress')
+let timer;
+function time(){
+	timer = setInterval(function(){
+		progress.value -= 1;
+		checkVitals()
+}, 1000);
 }
-	
 
-//let gameOn = true;
 
-let seconds = 30;
-let life = seconds;
+
+
+
 
 $('#start').click(function(){
-	//progress()
-	$('#lifePercentBar').css('display', 'none')
+	$('#progress').css('display', 'block')
 	$('.btn').css('display', 'none')
 	$('#start').css('display', 'none')
 	$('.questions').css('display', 'block')
+	$('h1').css('display', 'none')
+	time()
 })
 
 
 
 
 //Question One:
-	//correct answer
+	//correct answer Path
 $('#thomas').on('click', function(){
 	console.log('thomas has been clicked')
 	alert('Correct! You have survived this challenge.');
 	$('#qOne').css('display','none');
 	$('#qTwo').css('display','block');
+	progress.value += 1;
+})
+$()
+
+//Question Two:
+	//correct path
+$('#thoroHorse').on('click', function(){
+	console.log('thoroughbred has been clicked')
+	alert('Correct! You have survived this challenge.');
+	$('#qTwo').css('display','none');
+	$('#qThree').css('display','block');
+	progress.value += 1;
 })
 
-	//wrong answer, path 2
+$('#appaHorse').on('click', function(){
+	console.log('appaloosa was clicked')
+	alert('Sorry, wrong answer. The Thoroughbred is the fastest horse breed.');
+	progress.value -= 1;
+	$('#qTwo').css('display','none');
+	$('#qAlt1').css('display','block');
+})
+$('#shetland').on('click', function(){
+	console.log('shetland was clicked')
+	alert('Sorry, wrong answer. The Thoroughbred is the fastest horse breed.');
+	progress.value -= 1;
+	$('#qTwo').css('display','none');
+	$('#qAlt1').css('display','block');
+})
+//Question Three, right path:
+$('#goldRushYear').on('click', function(){
+	console.log('The gold rush year has been clicked')
+	alert('Correct! You have survived this challenge.');
+	$('#qThree').css('display','none');
+	$('#qFour').css('display','block');
+	progress.value += 1;
+})
 
+$('#wrongYear').on('click', function(){
+	console.log('incorrect year was clicked')
+	alert('Sorry, wrong answer. The gold rush started in Januarry of 1848.');
+	progress.value -= 2;
+	$('#qThree').css('display','none');
+	$('#qAlt2').css('display','block');
+})
+$('#wrongEra').on('click', function(){
+	console.log('incorrect year was clicked')
+	alert('Sorry, wrong answer. The gold rush started in Januarry of 1848.');
+	progress.value -= 1;
+	$('#qThree').css('display','none');
+	$('#qAlt2').css('display','block');
+})
+//Question four, right path
+	//berries or hunt
+$('#berries').on('click', function(){
+	$('#qThree').css('display','none');
+	$('#qFour').css('display', 'none')
+	$('#qAlt4').css('display','block');
+})
+$('#hunt').on('click', function(){
+	alert('Good choice, I was feeling more than just berries.')
+	$('#qThree').css('display','none');
+	$('#qFour').css('display', 'none')
+	$('#qFive').css('display','block');
+})
+
+		//animal hunting question
+$('#rightNoose').click(function(){
+	alert('Correct! You caught a rabbit! Time to eat.')
+	$('#qFive').css('display', 'none')
+	$('#finale').css('display', 'block')
+	progress.value += 1;
+	alert(`Congrats you made it with ${progress.value} seconds left!`)
+	clearInterval()
+
+})
+$('#smallNoose').click(function(){
+	alert('Oh no! Butch died due to starvation.')
+	gameOver()
+})
+$('#bigNoose').click(function(){
+	alert('Oh no! Butch died due to starvation.')
+	gameOver()
+})
+
+
+
+	//wrong answer, path 2
+		//president question
 	$('#adams').on('click', function(){
 		console.log('adams was clicked')
 		alert('Sorry, wrong answer. Thomas Jefferson was the third president.');
-		life =- 1;
+		progress.value -= 1;
 		$('#qOne').css('display','none');
 		$('#qAlt1').css('display','block');
 	})
 	$('#grant').on('click', function(){
 		console.log('grant was clicked')
 		alert('Sorry, wrong answer. Thomas Jefferson was the third president.');
-		life =- 1;
+		progress.value -= 1;
 		$('#qOne').css('display','none');
 		$('#qAlt1').css('display','block');
 	})
 
+			//stay or walk question
 	$('#stay').click(function(){
 		alert('Sorry, game over. Not too many travelers out here.');
-		//gameover()
+		gameOver()
 	})
 	$('#walk').click(function(){
 		$('#qAlt1').css('display', 'none')
 		$('#qAlt2').css('display', 'block');
+		progress.value += 1;
 	})
+
+		//walking N S E W question
 	$('.button.wrong').click(function(){
-		life =-1;
-		alert('Oh no! second wrong answer. Correct answer was South. Hopefully this next question is easier because I am getting hungry.');
+		progress.value -= 2;
+		alert('Oh no! Second wrong answer. Hopefully this next question is easier because I am getting cold.');
 			$('#qAlt2').css('display', 'none')
+			$('#qAlt4').css('display', 'none')
 			$('#qAlt3').css('display', 'block');
 	})
+	$('#south').click(function(){
+		alert('Correct!')
+		$('#qAlt2').css('display', 'none');
+		$('#qFour').css('display', 'block');
+		progress.value += 1;
+	})
+
+	// berries question
+	$('#rasp').click(function(){
+		alert('Sorry, Butch died from a weak stomach!')
+		gameOver()
+	})
+	$('#gold').click(function(){
+		alert('Sorry, Butch died from a weak stomach!')
+		gameOver()
+	})
+	$('#elderberries').click(function(){
+		alert('Correct!');
+		$('#qAlt4').css('display', 'none')
+		$('#qAlt5').css('display', 'block')	
+		progress.value += 1;
+	})
+
+
+		//building a fire
+	$('#dong').click(function(){
+		alert('Yes! Now we have a fire to cook our rabbit.')
+		$('#qAlt3').css('display', 'none')
+		$('#qAlt5').css('display', 'block')
+		progress.value += 1;
+	})
+	$('#chocolate').click(function(){
+		alert('Sorry, Butch died from the cold!')
+		gameOver()
+	})
+	$('#battery').click(function(){
+		alert('Sorry, Butch died from the cold!')
+		gameOver()
+	})
+
+	//sleep question
+	$('#uncomfortable').click(function(){
+		alert("Sorry, Butch died from hypothermia. Correct answer was to avoid loosing body heat.")
+		gameOver()
+	})
+	$('#bugs').click(function(){
+		alert("Sorry, Butch died from hypothermia. Correct answer was to avoid loosing body heat.")
+		gameOver()
+	})
+	$('#bodyHeat').click(function(){
+		$('#qAlt5').css('display', 'none')
+		$('#finalQ').css('display', 'block')
+		progress.value += 1;
+	})
+
+	//gun question
+	$('#colt').click(function(){
+		$('#finalQ').css('display', 'none')
+		$('#finale').css('display', 'block')
+		progress.value += 1;
+		clearInterval()
+	})
+	$('#rifle').click(function(){
+		alert("Oh nooo... they closed their doors. Looks like we'll have to keep walking...")
+		$('#qAlt6').css('display', 'none')
+		$('#qOne').css('display', 'block')
+	})
+	$('#pistol').click(function(){
+		alert("Oh nooo... they closed their doors. Looks like we'll have to keep walking...")
+		$('#finalQ').css('display', 'none')
+		$('#qOne').css('display', 'block')
+	})
 
 
 
+function checkVitals(){
+
+	if(progress.value === 0){
+		alert('Oh no Butch was out in the boonies for too long! Try again!')
+		gameOver();
+	}
+}
 
 
 
-
-//Question Two:
-
-$('#thoroHorse').on('click', function(){
-	console.log('thoroughbred has been clicked')
-	alert('Correct! You have survived this challenge.');
-	$('#qTwo').css('display','none');
-	$('#qThree').css('display','block');
-})
-
-$('#appaHorse').on('click', function(){
-	console.log('appaloosa was clicked')
-	alert('Sorry, wrong answer. The Thoroughbred is the fastest horse breed.');
-	life =- 1;
-})
-$('#shetland').on('click', function(){
-	console.log('shetland was clicked')
-	alert('Sorry, wrong answer. The Thoroughbred is the fastest horse breed.');
-	life =- 1;
-})
-
-//Question Three:
-$('#goldRushYear').on('click', function(){
-	console.log('The gold rush year has been clicked')
-	alert('Correct! You have survived this challenge.');
-	$('#qThree').css('display','none');
-	$('#qFour').css('display','block');
-})
-
-$('#wrongYear').on('click', function(){
-	console.log('incorrect year was clicked')
-	alert('Sorry, wrong answer. The gold rush started in Januarry of 1848.');
-	life =- 1;
-})
-$('#wrongEra').on('click', function(){
-	console.log('incorrect year was clicked')
-	alert('Sorry, wrong answer. The gold rush started in Januarry of 1848.');
-	life =- 1;
-})
-
+function gameOver(){
+	window.location.reload();
+	clearInterval()
+}
 
 
 
