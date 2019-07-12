ef34abd672ad948aec7cd2a559583e8d499d1812
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-07-12 12:29:06 -0600

    combined alerts and timebar update to left and right animate functions

--- js/app.js
+++ js/app.js
@@ -31,15 +31,11 @@ $('#start').click(function(){
 	time();
 })
 
-	
-
 
 //Question One:
 	//correct answer Path
 $('#thomas').on('click', function(){
 	animateLeft();
-	alert('Correct! You have survived this challenge.');
-	progress.value += 1;
 	$('#qOne').css('display','none');
 	$('#qTwo').css('display','block');
 })
@@ -47,50 +43,38 @@ $('#thomas').on('click', function(){
 //Question Two:
 	//correct path
 $('#thoroHorse').on('click', function(){
-	alert('Correct! You have survived this challenge.');
 	$('#firstCowboy').attr('src', "https://thumbs.gfycat.com/CaringAppropriateIndianpangolin-small.gif");
     animateLeft();
 	$('#qTwo').css('display','none');
 	$('#qThree').css('display','block');
-	progress.value += 1;
 })
 
 $('#appaHorse').on('click', function(){
-	alert('Sorry, wrong answer. The Thoroughbred is the fastest horse breed.');
-	progress.value -= 1;
 	animateRight();
 	$('#qTwo').css('display','none');
 	$('#qAlt1').css('display','block');
 })
 
 $('#shetland').on('click', function(){
-	alert('Sorry, wrong answer. The Thoroughbred is the fastest horse breed.');
-	progress.value -= 1;
 	animateRight()
 	$('#qTwo').css('display','none');
 	$('#qAlt1').css('display','block');
 })
 //Question Three, right path:
 $('#goldRushYear').on('click', function(){
-	alert('Correct! You have survived this challenge.');
 	animateLeft();
 	$('#qThree').css('display','none');
 	$('#qFour').css('display','block');
-	progress.value += 1;
 })
 
 $('#wrongYear').on('click', function(){
-	alert('Sorry, wrong answer. The gold rush started in Januarry of 1848.');
 	animateRight()
-	progress.value -= 1;
 	$('#qThree').css('display','none');
 	$('#qAlt2').css('display','block');
 })
 
 $('#wrongEra').on('click', function(){
-	alert('Sorry, wrong answer. The gold rush started in Januarry of 1848.');
 	animateRight();
-	progress.value -= 1;
 	$('#qThree').css('display','none');
 	$('#qAlt2').css('display','block');
 })
@@ -110,7 +94,7 @@ $('#hunt').on('click', function(){
 	$('#qFive').css('display','block');
 })
 
-		//animal hunting question
+		//Question Five, animal hunting, right path 
 $('#rightNoose').click(function(){
 	alert('Correct! You caught a rabbit! Time to eat.')
 	$('#qFive').css('display', 'none')
@@ -133,16 +117,12 @@ $('#bigNoose').click(function(){
 	//wrong answer, path 2
 		//president question
 $('#adams').on('click', function(){
-	alert('Sorry, wrong answer. Thomas Jefferson was the third president.');
-	progress.value -= 1;
 	animateRight();
 	$('#qOne').css('display','none');
 	$('#qAlt1').css('display','block');
 })
 
 $('#grant').on('click', function(){
-	alert('Sorry, wrong answer. Thomas Jefferson was the third president.');
-	progress.value -= 1;
 	animateRight();
 	$('#qOne').css('display','none');
 	$('#qAlt1').css('display','block');
@@ -158,25 +138,20 @@ $('#walk').click(function(){
 	animateLeft();
 	$('#qAlt1').css('display', 'none')
 	$('#qAlt2').css('display', 'block');
-	progress.value += 1;
 })
 
 		//walking N S E W question
 $('.button.wrong').click(function(){
-	progress.value -= 1;
 	animateRight()
-	alert('Oh no! Second wrong answer. Hopefully this next question is easier because I am getting cold.');
 	$('#qAlt2').css('display', 'none')
 	$('#qAlt4').css('display', 'none')
 	$('#qAlt3').css('display', 'block');
 })
 
 $('#south').click(function(){
-	alert('Correct!')
 	animateLeft();
 	$('#qAlt2').css('display', 'none');
 	$('#qFour').css('display', 'block');
-	progress.value += 1;
 })
 
 	// berries question
@@ -191,20 +166,16 @@ $('#gold').click(function(){
 })
 
 $('#elderberries').click(function(){
-	alert('Correct!');
 	animateLeft();
 	$('#qAlt4').css('display', 'none')
 	$('#qAlt5').css('display', 'block')	
-	progress.value += 1;
 })
 
 		//building a fire
 $('#dong').click(function(){
-	alert('Yes! Now we have a fire to cook our rabbit.')
 	animateLeft();
 	$('#qAlt3').css('display', 'none')
 	$('#qAlt5').css('display', 'block')
-	progress.value += 1;
 })
 
 $('#chocolate').click(function(){
@@ -229,11 +200,9 @@ $('#bugs').click(function(){
 })
 
 $('#bodyHeat').click(function(){
-	alert('Correct! Sleeping on the ground can cause hypothermia.')
 	animateLeft();
 	$('#qAlt5').css('display', 'none')
 	$('#finalQ').css('display', 'block')
-	progress.value += 1;
 })
 
 	//gun question
@@ -246,17 +215,19 @@ $('#colt').click(function(){
 });
 
 $('#rifle').click(function(){
-	alert("Oh nooo... they closed their doors. Looks like we'll have to keep walking...")
+	animateRight();
 	$('#finalQ').css('display', 'none')
 	$('#firstBackdrop').css('display', 'none');
 	$('#qOne').css('display', 'block')
 })
 
 $('#pistol').click(function(){
-	alert("Oh nooo... they closed their doors. Looks like we'll have to keep walking...")
+	animateRight();
 	$('#finalQ').css('display', 'none')
 	$('#qOne').css('display', 'block')
 })
+
+
 
 //exit game button
 $('#exitGame').on('click', function(){
@@ -269,14 +240,18 @@ $('#playAgain').on('click', function(){
 
 
 
-//animating img left, when player gets an answer right
+//answer question correclty, go left, add a second, and alert
 function animateLeft(){
+	alert('Correct!')
 	$("#firstCowboy" ).animate({ "left": "-=74px" }, "fast" );
+	progress.value += 1;
 }
 
-//animating img right, when player gets an answer wrong
+//answer question incorreclty, go right, subtrract a second, and alert
 function animateRight(){
+	alert('Oh no! Wrong answer. Try again!')
 	$( "#firstCowboy" ).animate({ "left": "+=36.67px" }, "fast" );
+	progress.value -= 1;
 }
 
 
